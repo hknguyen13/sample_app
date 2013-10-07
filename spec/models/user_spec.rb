c4ee633d0f903149e0b84d292b251104aef95a1c
Huu Nguyen <hknguyen13@gmail.com>
2013-10-07 19:18:50 -0400

    Changed font-weight of footer and trying to change main image responsivenss

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,7 +28,7 @@ describe User do
   end
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w{user@foo,com user_at_foo.org example.user@foo.
+      addresses = %w{user@foo,com foo@bar..com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com}
       addresses.each do |invalid_address|
         @user.email = invalid_address
@@ -89,5 +89,15 @@ describe User do
   describe "with a password that's too short" do
     before {@user.password = @user.password_confirmation = "a" * 5}
     it {should be_invalid}
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) {"Foo@ExAMPLe.Com"}
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 end
