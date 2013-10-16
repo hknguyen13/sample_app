4ccde5505f333fdc88358e2d808a263afdf7e98c
Huu Nguyen <hknguyen13@gmail.com>
2013-10-16 14:28:56 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "Huu Nguyen"
-    email "hnguyen14@gmail.com"
-    password "poopers1"
-    password_confirmation "poopers1"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
